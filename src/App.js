3e004a885ffa3992a4ca95eb2349296f30cd987a
rokaizen <robertojavier.araneda@gmail.com>
2021-04-28 01:43:30 -0400

    card

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
-import React from 'react';
-import {Container, AppBar , Typography, Grow, Grid} from '@material-ui/core'
+import React, { useEffect } from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
-import Post from './components/Posts/Posts.js'
-import Form from './components/Form/Form.js'
-import memories from './images/memories.png';
-import useStyles from './styles';
+import { getPosts } from "./actions/posts";
+import Post from "./components/Posts/Posts.js";
+import Form from "./components/Form/Form.js";
+import memories from "./images/memories.png";
+import useStyles from "./styles";
 
 const App = () => {
-    const classes = useStyles();
-    return(
-        <Container maxWidth='lg'>
-            <AppBar className={classes.appBar} position='static' color='inherit'>
-                <Typography className={classes.heading} variant='h2' alling='center'>JUJU</Typography>
-                <img className={classes.image} src={memories} alt='memories' height='60'/>
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container justify='space-between' alingItems='stretch' spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Post/>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
-    )
-}
+  const classes = useStyles();
+  const dispatch = useDispatch();
 
-export default App
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" alling="center">
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Post />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import Post from './Post/Post.js'
+import React from "react";
+import { useSelector } from "react-redux";
 
-const Posts =  () =>{
-    return (
-        <>
-        <h1>Posts</h1>
-        <Post/>
-        <Post/>
-        <Post/>
-        </>
-    );
-}
+import Post from "./Post/Post.js";
+const Posts = () => {
+  const posts = useSelector((state) => state.posts);
+
+  console.log(posts);
+  return (
+    <>
+      <h1>Posts</h1>
+      <Post />
+      <Post />
+      <Post />
+    </>
+  );
+};
 
 export default Posts;
